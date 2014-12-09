09d9e16531ae103c18b994271e450db77e381bbc
Lee Farrell <lee.farrell@clavisinsight.com>
2014-12-09 16:23:30 +0000

    fix landscape mode

--- javascripts/main-built.js
+++ javascripts/main-built.js
@@ -15603,25 +15603,25 @@ define('game_screen',["Player", "Point", "game", "Settings", "Gamevars"], functi
       usingLandscape = true;
       if (usingLandscape) {
         if (Gamevars.accelerometerY > 0.05) {
-          currentPlayer.accelY = maxPlayerAccel;
+          currentPlayer.accelX = -maxPlayerAccel;
         } else if (Gamevars.accelerometerY < -0.05) {
-          currentPlayer.accelY = -maxPlayerAccel;
+          currentPlayer.accelX = maxPlayerAccel;
         }
         if (Gamevars.accelerometerX < -0.05) {
-          currentPlayer.accelX = maxPlayerAccel;
+          currentPlayer.accelY = -maxPlayerAccel;
         } else if (Gamevars.accelerometerX > 0.05) {
-          currentPlayer.accelX = -maxPlayerAccel;
+          currentPlayer.accelY = maxPlayerAccel;
         }
       } else {
         if (Gamevars.accelerometerY > 0.05) {
-          currentPlayer.accelX = -maxPlayerAccel;
+          currentPlayer.accelY = maxPlayerAccel;
         } else if (Gamevars.accelerometerY < -0.05) {
-          currentPlayer.accelX = maxPlayerAccel;
+          currentPlayer.accelY = -maxPlayerAccel;
         }
         if (Gamevars.accelerometerX < -0.05) {
-          currentPlayer.accelY = -maxPlayerAccel;
+          currentPlayer.accelX = maxPlayerAccel;
         } else if (Gamevars.accelerometerX > 0.05) {
-          currentPlayer.accelY = maxPlayerAccel;
+          currentPlayer.accelX = -maxPlayerAccel;
         }
       }
       currentPlayer.speedX += currentPlayer.accelX * delta;
